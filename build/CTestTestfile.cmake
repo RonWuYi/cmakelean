0a95454bc060fd5585f15c8c9e4158c1024dd965
RonWuYi <lyshmily@outlook.com>
2021-03-10 22:29:09 +0100

    update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hdc/Downloads/project/github/cplus/cmakelean
+# Build directory: /home/hdc/Downloads/project/github/cplus/cmakelean/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,50 @@
+// thread example
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+#include <cstdlib>         
+#include <ctime>
+#include <future>         // std::
+
+
+void foo() 
+{
+  // do stuff...
+}
+
+void bar(int x)
+{
+  // do stuff...
+}
+
+struct foo
+{
+    int a;
+    int b;
+    /* data */
+} my_foo;
+
+
+void long_operation()
+{
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(150ms);
+}
+int main(){
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
+    auto t1 = high_resolution_clock::now();
+
+    long_operation();
+
+    auto t2 = high_resolution_clock::now();
+
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+    duration<double, std::milli> ms_double = t2 - t1;
+    std::cout << ms_int.count() << "ms\n";
+    std::cout << ms_double.count() << "ms";
+    return 0;
+}
