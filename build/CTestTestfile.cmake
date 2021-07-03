f0dad07bda6f737fb4b98038116e331b1b10ae83
RonWuYi <lyshmily@outlook.com>
2021-07-03 07:16:58 -0400

    update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hdc/project/github/cmakelean
-# Build directory: /home/hdc/project/github/cmakelean/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
