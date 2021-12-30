dd4dc0a02af5171993f9e334595a5af9b6232f49
RonWuYi <lyshmily@outlook.com>
2021-12-30 10:19:14 +0100

    change main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,14 @@
 
 int main() {
     std::cout << "Bla: " << Formula::bla(2) << std::endl;
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    std::string s{"Hello"};
+    std::cout << "Hello: " << s.size() << std::endl;
+    std::cout << "Hello: " << s.length() << std::endl;
+    v.push_back(6);
+    for (auto i : v) {
+        std::cout << i << std::endl;
+    }
+    std::for_each(v.rbegin(), v.rend(), [](const int n) { std::cout << n << ' '; });
     return 0;
 }
