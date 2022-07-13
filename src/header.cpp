fa261145db19f53074859193d0d269b8ece50c31
RonWuYi <lyshmily@outlook.com>
2022-07-13 17:34:21 +0200

    update

--- src/header.cpp
+++ src/header.cpp
@@ -9,14 +9,10 @@ void putCharQueueBig(charQueue &queue)
 {
     std::lock_guard<std::mutex> guard(g_pages_mutex);
     auto temp = std::make_unique<charData>();
-    // std::fill(temp.get()->begin(), temp.get()->end(), 'a');
 
     for (size_t i = 0; i < BIG; i++)
     {
-        /* code */
-        // temp.get()->at(i) = 'a' + rand()%26;
         auto p = 'a' + rand()%26;
-        // temp.get()[0][i] = p;
     }
 }
 
@@ -24,48 +20,12 @@ void putIntQueueBig(intQueue &queue)
 {
     std::lock_guard<std::mutex> guard(g_pages_mutex);
     auto temp = std::make_unique<intData>();
-    // auto length = 0;
-
-    // while (true) {
-
-    // }
 
     for (int i = 0; i < BIG; i++)
     {
-        /* code */
         temp->at(i) = rand()%1000;
-        // length++;
     }
-
-    // while (true) {
-    //     if (temp.get()[0][length] == temp.get()->end()) {
-    //         // temp->at(length) = rand()%1000;
-    //         // length++;
-    //         break;
-    //     }
-    // }
-
-    // while (true)
-    // {
-    //     if (temp.get()[0][length] == temp.get()[0].end())
-    //     {
-    //         // length++;
-    //         break;
-    //     }
-    //     // else
-    //     // {
-    //     //     break;
-    //     // }))
-    //     // {
-    //     //     break;
-    //     // }
-    //     // auto length = temp.get()[0]->size();
-    //     // temp.get()[0][length] = rand()%100;
-    //     temp->at(0)[0] = rand()%100;
-    //     length++;
-    // }
     queue.push(std::move(temp));
-
 }
 
 void getIntQueueBig(intQueue &queue)
@@ -76,29 +36,11 @@ void getIntQueueBig(intQueue &queue)
     std::cout << "queue size: " << p << std::endl;
 }
 
-// void popIntQueueBig(intQueue &queue, intQueueSmall &queueSmall)
-// {
-//     std::lock_guard<std::mutex> guard(g_pages_mutex);
-
-//     if (!queue.empty())
-//     {
-//         auto temp = queue.front();
-//         queue.pop();
-//     }
-// }
 int factorial(int n) {
 	if (n < 1) return 1;
 	else return n * factorial(n - 1);
 }
 
-// template <class T>
-// T mysum(T a[], int n) {
-// 	T theSum = 0;
-// 	for (int i = 0; i < n; i++) {
-// 		theSum += a[i];
-// 	}
-// 	return theSum;
-// }
 int Fibonacci(int n)
 {
     if (n == 0)
@@ -127,27 +69,8 @@ int SolutionwallsAndGates::distanceTonearestGates(std::vector<std::vector<int>>&
 
     std::vector<std::vector<int>> distance{};
     std::queue<std::array<int, 2>> q{};
-    // auto temp = std::array<int, 2>(startRow, startCol);
-    // q.push(temp);
-    // while (!q.empty()) {
-    //     auto temp = q.front();
-    //     q.pop();
-    //     int row = temp[0];
-    //     int col = temp[1];
-    //     for (auto& direction: DIRECTIONS)
-    //     {
-    //         int r = row + direction[0];
-    //         int c = col + direction[1];
-
-    //         // if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] == WALL
-    //         //         || direction[r][c] != 0)
-    //         //         continue;
-    //     }
-    // }
     return 0;
-
 }
-
 
 void SolutionwallsAndGatesNew::wallsAndGates(vector<vector<int>>& rooms) {
     const int row = rooms.size();
