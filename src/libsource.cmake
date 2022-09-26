db171981b3005bffb481ae742f0aa2486cc2f9f7
ron.wu <ron.wu@irdeto.com>
2022-09-26 09:25:24 +0200

    update cmake file

--- src/libsource.cmake
+++ src/libsource.cmake
@@ -1,1 +1,10 @@
-set(libsoure ${CMAKE_CURRENT_LIST_DIR}/components.cpp)
+message(STATUS "from src/libsource.cmake CMAKE_CURRENT_LIST_DIR is ${CMAKE_CURRENT_LIST_DIR}")
+set(libsoure ${CMAKE_CURRENT_LIST_DIR}/components.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/Formula.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/header.cpp)
+
+
+set(mainappsoure ${CMAKE_CURRENT_LIST_DIR}/components.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/Formula.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/header.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/main.cpp)
